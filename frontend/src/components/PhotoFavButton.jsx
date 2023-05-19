7dfa1921d1a07fa6a2d6ac0e09bd8548e6560be7
hoomili <hasadian@hotmail.com>
2023-05-18 20:34:09 -0700

    adding favorite button to each picture

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,16 @@ import { FavIcon } from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+  const [favorite, setFavorite] = useState('off')
+  const favoriteChange = () => {
+    setFavorite(favorite === "on" ? "off" : "on")
+    console.log(favorite);
+  }
   return (
     <div className="photo-list--fav-icon">
-      <div className="photo-list--fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list--fav-icon-svg" onClick={favoriteChange}>
+        {favorite === "on" && <FavIcon fill={'#ff0000'}/>}
+        {favorite === "off" && <FavIcon/>}
       </div>
     </div>
   );
