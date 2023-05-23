7916e41b6df2ada696b5dd4f2c485068e45f605c
hoomili <hasadian@hotmail.com>
2023-05-22 17:24:16 -0700

    added instructions for using reducer

--- frontend/src/hooks/useApplicationData.jsx
+++ frontend/src/hooks/useApplicationData.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 
 const photoArr = [];
 const useApplicationData = () => {
@@ -42,5 +42,29 @@ const useApplicationData = () => {
     updateToFavPhotoIds
   }
   return allStates;
+
+  
 }
+
+
+// export const ACTIONS = {
+//   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+//   FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+//   SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+//   SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+//   SELECT_PHOTO: 'SELECT_PHOTO',
+//   DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+// }
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case FAV_PHOTO_ADDED:
+//       return { /* insert logic */ }
+//     { /* insert all relevant actions as case statements*/ }  
+//     }
+//     default:
+//       throw new Error(
+//         `Tried to reduce with unsupported action type: ${action.type}`
+//       );
+//   }
 export default useApplicationData
